94e10e6fc6692ed0fd1842e0fa326c5aca4e4923
Aleks Kudryashov <aleks59niki30@gmail.com>
2021-08-30 21:42:46 +0300

    task completed

---
+++ app/views.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+from django.shortcuts import render, reverse
+
+from datetime import datetime
+
+import os
+
+
+def home_view(request):
+    template_name = 'app/home.html'
+
+    pages = {
+        'Главная страница': reverse('home'),
+        'Показать текущее время': reverse('time'),
+        'Показать содержимое рабочей директории': reverse('workdir')
+    }
+
+    context = {
+        'pages': pages
+    }
+    return render(request, template_name, context)
+
+
+def time_view(request):
+    current_time = datetime.today().strftime("%H:%M:%S")
+    msg = f'Текущее время: {current_time}'
+    return HttpResponse(msg)
+
+
+def workdir_view(request):
+    msg = '<br>'.join(os.listdir())
+    return HttpResponse(msg)
